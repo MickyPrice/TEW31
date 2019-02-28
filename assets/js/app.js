88c6d9c372d0d32dd008e340698b0e2cd2f7abc5
Michael Price <realMPrice@gmail.com>
2019-02-28 21:15:04 +1300

    Added html content loading to lessons

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,17 @@ function loadLesson(lessonID) {
   getJsonFile(`/app/lessons/${lessonID}/manifest.json`, data => {
     // do something with your data
     if (data !== "404") {
-      findIdentifier("title").innerText = data.title
+      findIdentifier("title").innerText = data.title;
+      findIdentifier("header").style.backgroundImage = `url("${data.image}")`;
+
+      getHTMLFile(`/app/lessons/${lessonID}/content.html`, data => {
+        if (data !== "404") {
+          findIdentifier('lessonContent').innerHTML = data;
+        }else {
+          alert("Error: Failed to load lesson content")
+        }
+      })
+
     }else {
       location.href = "/app"
     }
@@ -33,6 +43,20 @@ function getJsonFile(path, callback) {
   httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState === 4 && httpRequest.status === 200) {
       var data = JSON.parse(httpRequest.responseText);
+      if (callback) callback(data);
+    }
+    if (httpRequest.readyState === 4 && httpRequest.status === 404) {
+      if (callback) callback("404")
+    }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send();
+}
+function getHTMLFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+      var data = httpRequest.responseText;
       if (callback) callback(data);
     }
     if (httpRequest.readyState === 4 && httpRequest.status === 404) {
